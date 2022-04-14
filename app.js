ccfb0269ff1441c57829071f7aeffa17339cd24c
mayragann <mayragann@gmail.com>
2022-04-14 10:19:24 -0400

    Compleated Question 8

--- app.js
+++ app.js
@@ -234,6 +234,19 @@ let foodDescription = problemSeven();
 console.log("Dish Descriptiopn: ", foodDescription)
 //8. Create a function that will append the cuisine type to the start of the dish's name. Then, return only the Vegetarian dish objects. So this function should return objects 11-13 with their names changed to "Vegetarian Lasagna", "Vegetarian Falafel", "Vegetarian Chili"
 //Map, Filter
+function problemEight(){
+  let results = dishes.filter(function (el) {
+    return el.cuisine === "Vegetarian";
+}).map(function (el) {
+    return el.cuisine + " " + el.name;
+})
+
+    return results ;
+
+}
+let vegetarianFoodOnly = problemEight();
+console.log("Dish Descriptiopn and Vegetarian: ", vegetarianFoodOnly)
+
 
 //BONUS
 //9. Create a function that will return dishes whose ingredients array INCLUDES "chickpea".
